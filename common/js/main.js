91f9ce0e27a255871221f5b2b8b87190534da35c
13eom <91525346+13eom@users.noreply.github.com>
2023-08-08 07:54:58 +0900

    Add files via upload

--- common/js/main.js
+++ common/js/main.js
@@ -63,7 +63,7 @@ $(function(){
 	var pf_list_w = pf_list.children('ul.tit_list');
 
 	$.ajax({
-		url : "../common/ajax/project.json",
+		url : "../ajax/project.json",
 		type : "get",
 		dataType: "json",
 		success : function(data) {
